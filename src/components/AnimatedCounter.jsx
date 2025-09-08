a635984c8d8a6f414f1fa95324accee18d6f123f
Ndiooro <ndioro003@gmail.com>
2025-09-08 15:12:43 +0000

    Animate showcase section : attempt 1

--- src/components/AnimatedCounter.jsx
+++ src/components/AnimatedCounter.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 import { div } from 'three/tsl'
 import { counterItems } from '../constants'
 import CountUp from 'react-countup'
-
+import { useGSAP } from '@gsap/react';
+import {gsap} from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 const AnimatedCounter = () => {
+ 
   return (
    <div id="counter" className='padding-x-lg xl:mt-0 mt-20'>
     <div className="mx-auto grid-3-cols-md-notsotight">
-      {counterItems.map((item)=>(
-        <div className='bg-zinc-900 rounded-lg p-10 flex flex-col justify-center'>
-        <div key={counterItems.label} className=" text-white text-5xl font-bold mb-2">
-        <CountUp suffix={item.suffix} end={item.value}  />
-       </div>
-       <div className='text-white-50 text-lg'>{item.label}</div>
+      {counterItems.map((item) => (
+        <div key={item.label} className='bg-zinc-900 rounded-lg p-10 flex flex-col justify-center'>
+          <div className="text-white text-5xl font-bold mb-2">
+            <CountUp suffix={item.suffix} end={item.value} />
+          </div>
+          <div className='text-white-50 text-lg'>{item.label}</div>
         </div>
-       
       ))}
     </div>
    </div>
--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -1,24 +1,105 @@
-import React from 'react'
+import React, { use } from 'react'
+import { useRef } from 'react';
+import { useGSAP } from '@gsap/react';
+import {gsap} from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin();
 
 const ShowcaseSection = () => {
+    const sectionRef= useRef(null);
+    const project1Ref= useRef(null);
+    const project2Ref= useRef(null);
+    const project3Ref= useRef(null);
+
+    
+    useGSAP(() => {
+        const projects=[project1Ref.current, project2Ref.current, project3Ref.current];
+
+    projects.forEach((card, index) => {
+        gsap.fromTo(
+            card,
+            {
+                y: 50, opacity:0
+            },
+            {
+                y:0,
+                opacity:1,
+                duration:1,
+                delay: 0.3 * (index +1),
+                scrollTrigger:{
+                    trigger: card,
+                    start: 'top bottom-=100',
+                    }
+            }
+        )
+    }
+)
+        gsap.fromTo(sectionRef.current, {
+            opacity: 0,
+          }, {
+            opacity: 1,
+            duration: 2,
+            scrollTrigger: {
+              trigger: sectionRef.current,
+              start: 'top bottom',
+              end: 'bottom top',
+              scrub: true,
+            },
+          });
+
+    }, []);
   return (
-    <div id="work" className="app-showcase">
+    <section id="work" ref={sectionRef} className="app-showcase">
         <div className="w-full">
-        <div className="showcaselayout"></div>
-        {/*LEFT */}
-        <div className="first-project-wrapper">
+        <div className="showcaselayout">
+         {/*LEFT */}
+        <div className="first-project-wrapper" ref={project1Ref}>
             <div className="image-wrapper">
-            <img src="/images/project1.png" alt="Ryde" />
+            <img src="/images/Programme.png" alt="Ryde" />
             </div>
             <div className='text-content'>
-            <h2>Ryde</h2>
+            <h2>Oasis Care</h2>
             <p className="text-white-50 md:text-xl">
-                
+                Une application de télémédecine spécialisée dans le suivi des personnes atteintes de diabète
             </p>
             </div>
         </div>
+         {/*RIGHT */}
+       <div className="project-list-wrapper overflow-hidden" >
+           <div className='project' ref={project2Ref}>
+             <div className="image-wrapper ">
+            <img src="/images/project2.png" alt="Ryde" />
+            </div>
+            <div className='text-content'>
+            <h2>Oasis Care</h2>
+            <p className="text-white-50 md:text-xl">
+                Une application de télémédecine spécialisée dans le suivi des personnes atteintes de diabète
+            </p>
+            </div>
+            </div> 
+
+                 <div className='project' ref={project3Ref}>
+             <div className="image-wrapper ">
+            <img src="/images/project2.png" alt="Ryde" />
+            </div>
+            <div className='text-content'>
+            <h2>Oasis Care</h2>
+            <p className="text-white-50 md:text-xl">
+                Une application de télémédecine spécialisée dans le suivi des personnes atteintes de diabète
+            </p>
+            </div>
+            </div> 
         </div>
-    </div>
+        </div>
+
+        </div>
+        </section>
+
+        
+      
+    
+  
   )
 }
 
